9094b341de870f04e9777a85cf3be3d226771b83
Joe Fainer <joef987@gmail.com>
2016-04-05 20:22:19 -0400

    Latest version  Fix A* and Waypoints

--- lab3/scripts/lab3_grid_cellsD16.py
+++ lab3/scripts/lab3_grid_cellsD16.py
@@ -84,12 +84,15 @@ def aStar(start,goal):
 
 	i = 0
 	while queue:
+                showCells(visited, 3)
 		node, cost, path = queue.pop(0)
 
 		if node == goal:
 			print 'Found path: ', path
 			print 'cost: ', cost
-			showCellsVisited(path, visited)
+                        showCells(wayPoints(path), 3)
+			showCells(path, 2)
+                        showCells(wayPoints(path), 3)
 			#for n in path:
 				#print isWall(n)
 			break
@@ -139,94 +142,14 @@ def aStar(start,goal):
 				else:
 					queue[i] = next_item
 				visited.append(next_node)
-'''
-def aStar(start,goal):
-	print 'Staring AStar'
-	print 'Width: %d' % width
-	print 'Height %d' % height
-	print 'Start X: %d' % start[0]
-	print 'Start Y: %d' % start[1]
-	print 'Goal X: %d' % goal[0]
-	print 'Goal Y: %d' % goal[0]
-	closedset = []    # The set of nodes already evaluated.
-	openset = [start]            # The set of tentative nodes to be evaluated, initially containing the start node.  The nodes in this set are the nodes that make the frontier between the closed set and all other nodes.
-	came_from = [[]]    # The map of navigated nodes.
-	for i in range(width):
-		list = []
-		for j in range(height):
-			list.append(None)
-		came_from.append(list)
-	came_from[start[0]][start[1]] = None
-	
-	# The g_score of a node is the distance of the shortest path from the start to the node.
-	# Start by assuming that all nodes that have yet to be processed cannot be reached 
-	g_score = [[]]
-	for i in range(width):
-		list = []
-		for j in range(height):
-			list.append(10000000)
-		g_score.append(list)
-
-	
-	# The starting node has zero distance from start
-	g_score[start[0]][start[1]] = 0
-	
-    # The f_score of a node is the estimated total cost from start to goal to the goal.  This is the sum of the g_score (shortest known path) and the h_score (best possible path).
-    # assume same as g_score
-	f_score = [[]]
-	for i in range(width):
-		list = []
-		for j in range(height):
-			list.append(10000000)
-		f_score.append(list)
-	# heuristic_cost_estimate(a, b) is the shortest possible path between a and b, this can be euclidean, octodirectional, Manhattan, or something fancy based on how the machine moves
-	# the best possible distance between the start and the goal will be the heuristic
-	f_score[start[0]][start[1]] = g_score[start[0]][start[1]] + heuristic_cost_estimate(start, goal)
-     
-	current = 0
-	while (len(openset) > 0):
-		showCells(closedset)
-		print 'Exploring...'                                         # while there are still nodes that have not been checked, continually run the algorithm
-		for i in range(len(openset)):
-			if f_score[openset[i][0]][openset[i][1]] > current:
-				current = [openset[i][0], openset[i][1]]			# this is the most promising node of all nodes in the open set
-		if current == goal:                                           # if the best possible path found leads to the goal, it is the best possible path that the robot could discover
-			return reconstruct_path(came_from, goal)
-
-		if current in openset:
-			openset.remove(current)                  # mark this node as having been evaluated
-		closedset.append(current) 
-
-		neighbors = neighbor_nodes(current)
-		for i in range(len(neighbors)): 				# re-evaluate each neighboring node
-			if neighbors[i] in closedset:
-				continue
-			tentative_g_score = g_score[current[0]][current[1]] + dist_between(current,neighbors[i])	# create a new g_score for the current neighbor by adding the g_score from the current node and
-			                                                                      					# the distance to the neighbor
- 
-			if neighbors[i] not in openset or tentative_g_score < g_score[neighbors[i][0]][neighbors[i][1]]:                # if the neighbor has not been evaluated yet, or if a better path to the neighbor has been found,
-				                                                                                							# update the neighbor
-				came_from[neighbors[i][0]][neighbors[i][1]] = current                                                   							# The node to reach this node from in the best time is the current node
-				g_score[neighbors[i][0]][neighbors[i][1]] = tentative_g_score                                           							# The G score of the node is what we tentatively calculated earlier
-				f_score[neighbors[i][0]][neighbors[i][1]] = g_score[neighbors[i][0]][neighbors[i][1]] + heuristic_cost_estimate(neighbors[i], goal) 							# The F score is the G score and the heuristic
-				if neighbors[i] not in openset:                                                      							# add this neighbor to the frontier if it was not in it already
-					openset.append(neighbors[i])
-
-	print "There is not a possible path"
-'''
-	#if the program runs out of nodes to check before it finds the goal, then a solution does not exist
-
-# Starting from the goal, work backwards to find the start.  We recommend returning a path nav_msgs, which is an array of PoseStamped with a header
-    # create a new instance of the map
-
-    # generate a path to the start and end goals by searching through the neighbors, refer to aStar_explanied.py
-
-    # for each node in the path, process the nodes to generate GridCells and Path messages
-
-    # Publish points
-
-#publishes map to rviz using gridcells type
-
+def wayPoints(path):
+    points = list()
+    for i in range(len(path)-1):
+        current = math.atan2(path[i][1] - path[(i+1)][1], path[i][0] - path[(i+1)][0]) #find the angle between the current and next points
+        if (i != 0) and (current != last): #if the angle has changed
+            points.append(path[i]) #add the point to the list of waypoints
+        last = current
+    return points
 
 def reconstruct_path(came_from,current):
 
@@ -301,14 +224,14 @@ def publishCells(grid, num):
     	pubway.publish(cells)          
 
 
-def showCells(list):
+def showCells(list, num):
 	grid = []
 	for i in range(width + 1):
 		for j in range(height + 1):
 			grid.append(0)
 	for i in range(len(list)):
 		grid[(width+1)*list[i][0] + list[i][1]] = 100
-	publishCells(grid, 2)
+	publishCells(grid, num)
 
 def showCellsVisited(list, visited):
 	grid = []
@@ -320,6 +243,8 @@ def showCellsVisited(list, visited):
 	for i in range(len(list)):
 		grid[(width+1)*list[i][0] + list[i][1]] = 100
 	publishCells(grid, 3)
+
+
 
 
 #Main handler of the project
@@ -338,7 +263,7 @@ def run():
     # wait a second for publisher, subscribers, and TF
     rospy.sleep(1)
     start = [20, 20]
-    goal = [160, 160]
+    goal = [150, 150]
     # showCells([start, goal])
     #publishCells(mapData, 3)
     #path = aStar(start, goal)
@@ -348,8 +273,9 @@ def run():
     #     publishCells(mapData) #publishing map data every 2 seconds
     #     rospy.sleep(2)  
     #     print("Complete")
-
+    showCells([start, goal], 1)
     aStar(start,goal)
+    showCells(wayPoints(path), 3)
     
 
 
